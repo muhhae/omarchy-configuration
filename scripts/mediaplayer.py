c4032092d224141cc6c33d87852c8fbcfbb1f8b7
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2026-01-29 16:07:10 +0700

    perf: change mediaplayer scripts to event based

--- scripts/mediaplayer.py
+++ scripts/mediaplayer.py
@@ -7,37 +7,6 @@ import json
 import html
 
 
-def get_player_data():
-    try:
-        data = subprocess.run(
-            [
-                "playerctl",
-                "-p",
-                "spotify,%any",
-                "metadata",
-                "--format",
-                "{{status}}|{{title}}|{{artist}}|{{mpris:length}}|{{position}}|{{volume}}",
-            ],
-            capture_output=True,
-            text=True,
-            check=True,
-        ).stdout.strip()
-        if data:
-            parts = data.split("|")
-            metadata = {
-                "status": parts[0],
-                "title": parts[1],
-                "artist": parts[2],
-                "length": int(parts[3]) if parts[3] else 0,
-                "position": int(parts[4]) if parts[4] else 0,
-                "volume": float(parts[5]) if parts[5] else 0.0,
-            }
-            return metadata
-
-    except Exception:
-        return None
-
-
 bar_len = 10
 
 fill_char_passed = "━"
@@ -64,43 +33,60 @@ icon = ""
 # "format-paused": " ⏸  {title} - {artist}",
 
 
-while True:
-    data = get_player_data()
-    output = {}
+playerctl = subprocess.Popen(
+    [
+        "playerctl",
+        "-p",
+        "spotify,%any",
+        "metadata",
+        "--format",
+        "{{status}}|{{title}}|{{artist}}|{{mpris:length}}|{{position}}|{{volume}}",
+        "--follow",
+    ],
+    stdout=subprocess.PIPE,
+    text=True,
+)
 
-    if data:
-        if data.get("length", 0) > 0:
-            pct = min(1.0, max(0.0, data["position"] / data["length"]))
-        else:
-            pct = 0
-
-        spot_to_fill = bar_len - 1
-        filled_count = round(pct * spot_to_fill)
-        empty_count = spot_to_fill - filled_count
-
-        status = icon if data["status"] == "Playing" else "⏸"
-        progress_bar = (
-            border_left
-            + fill_char_passed * filled_count
-            + indicator
-            + fill_char_progress * empty_count
-            + border_right
-        )
-        position_str = time.strftime("%M:%S", time.gmtime(data["position"] / 1_000_000))
-        length_str = time.strftime("%M:%S", time.gmtime(data["length"] / 1_000_000))
-
-        output["text"] = html.escape(
-            f" {status}  {position_str} {progress_bar} {length_str}  {data['title']} - {data['artist']}"
-        )
-        output["tooltip"] = (
-            "left-click: play/pause\n"
-            + "right-click: next song\n"
-            + "middle-click: previous song\n"
-            + "scroll: volume+/-"
-        )
+for line in playerctl.stdout:
+    parts = line.split("|")
+    metadata = {
+        "status": parts[0],
+        "title": parts[1],
+        "artist": parts[2],
+        "length": int(parts[3]) if parts[3] else 0,
+        "position": int(parts[4]) if parts[4] else 0,
+        "volume": float(parts[5]) if parts[5] else 0.0,
+    }
+    if metadata.get("length", 0) > 0:
+        pct = min(1.0, max(0.0, metadata["position"] / metadata["length"]))
     else:
-        output["text"] = "  No Player"
+        pct = 0
+
+    spot_to_fill = bar_len - 1
+    filled_count = round(pct * spot_to_fill)
+    empty_count = spot_to_fill - filled_count
+
+    status = icon if metadata["status"] == "Playing" else "⏸"
+    progress_bar = (
+        border_left
+        + fill_char_passed * filled_count
+        + indicator
+        + fill_char_progress * empty_count
+        + border_right
+    )
+    position_str = time.strftime("%M:%S", time.gmtime(metadata["position"] / 1_000_000))
+    length_str = time.strftime("%M:%S", time.gmtime(metadata["length"] / 1_000_000))
+
+    output = {}
+    output["text"] = html.escape(
+        f" {status}  {position_str} {progress_bar} {length_str}  {metadata['title']} - {metadata['artist']}"
+    )
+    output["tooltip"] = (
+        "left-click: play/pause\n"
+        + "right-click: next song\n"
+        + "middle-click: previous song\n"
+        + "scroll: volume+/-"
+    )
 
     print(f"{json.dumps(output, ensure_ascii=False)}")
     sys.stdout.flush()
-    time.sleep(1)
