a88ff8cf8b5e5272739ef8565a48e1a75a3f6b33
Muhammad Haekal M. A. <haekal.muhamat@gmail.com>
2026-02-17 19:00:48 +0700

    feat: disable text wrap and enable relative number

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,7 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.opt.showtabline = 0
 vim.opt.laststatus = 0 -- Optional: also hides the statusline
 vim.opt.swapfile = false
+vim.opt.wrap = false
+-- vim.opt.textwidth = 80
+-- vim.opt.formatoptions:append("t")
